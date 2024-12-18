c7cfa61866678a98149fcaa512987cfc7ee9abda
Wassim Daoudi Idrissi <wdaoudi-@bess-f3r1s3.clusters.42paris.fr>
2024-12-18 19:47:15 +0100

    push pour la maison

--- test/time.c
+++ test/time.c
@@ -27,10 +27,33 @@ int	main(void)
 		- start_time);
 	while (get_time_in_ms() < end_time)
 	{
-		usleep(100 * 1000); // Suspend for 100 milliseconds
-		time = get_time_in_ms();
-		printf("Time in ms = %ld. Time since start = %ld\n", time, time
-			- start_time);
+		if (usleep(100 * 1000) == 0) // Suspend for 100 milliseconds
+		{
+			time = get_time_in_ms();
+			printf("Time in ms = %ld. Time since start = %ld\n", time, time
+				- start_time);
+		}
 	}
 	return (0);
+}
+
+// interrompre usleep
+
+void	philo_sleep(t_table *table, time_t sleep_time)
+{
+	// Variable to measure when the philosopher thread must resume:
+	time_t	wake_up;
+
+	wake_up = get_time_in_ms() + sleep_time;
+        // Loop as long as it's not wake up time:
+	while (get_time_in_ms() < wake_up)
+	{
+		// Check if the simulation has stopped. If it has,
+		// stop the loop (and this function) immediately
+		// to continue thread execution:
+		if (has_simulation_stopped(table))
+			break ;
+		// If the simulation is still going, usleep for a short time:
+		usleep(100);
+	}
 }
